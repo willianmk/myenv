a5b4bbe8cb56045f8e9c7377b97ea1f151cecc3a
Willian Kumatsu <willian.kumatsu@flaconi.de>
2021-04-15 14:46:50 +0200

    Added installation script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# Set zsh and oh-my-zsh
+chsh -s /usr/bin/zsh root
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+cp .zsh ~/
+
+#nvim configuration
+cp -r ./.config ~/
+mkdir -p ~/.config/nvim/bundle/
+cd ~/.config/nvim/bundle/
+git clone https://github.com/gmarik/Vundle.vim.git
+nvim +PluginInstall +qa!
+cd ~/
